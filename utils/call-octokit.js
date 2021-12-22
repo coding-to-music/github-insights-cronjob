b3c2336813310920cef9fd5bc5bb500de65d5fc2
Yadong Zhang <zhyd007@gmail.com>
2021-12-22 20:36:38 +0800

    fix: update env variables.

--- utils/call-octokit.js
+++ utils/call-octokit.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Octokit } = require("@octokit/core");
 
 const callOctokit = async (route, owner, repo) => {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+  const octokit = new Octokit({ auth: process.env.OCTOKIT_TOKEN });
   return await octokit.request(route, { owner, repo });
 }
 
